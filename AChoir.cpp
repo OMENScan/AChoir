c652f6c7937eec8ab0c4e4066b00f09129d1d576
OMENScan <OMENScan@gmail.com>
2019-01-20 08:15:57 -0800

    Re-Sync correct version
    Fix weird merge branch master thing...

--- AChoir.cpp
+++ AChoir.cpp
@@ -7978,14 +7978,6 @@ VOID ReadAttribute(PATTRIBUTE attr, PVOID buffer)
 
   if (attr->Nonresident == FALSE)
   {
-
-
-
-
-printf("Resident File\n");
-
-
-
     rattr = PRESIDENT_ATTRIBUTE(attr);
     memcpy(buffer, Padd(rattr, rattr->ValueOffset), rattr->ValueLength);
   }
